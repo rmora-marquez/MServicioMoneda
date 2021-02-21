ca162a9c04d5b50d27952e07fca30cbb4c60fa6d
Ramo Mora Marquez <rmora.marquez@gmail.com>
2021-02-20 20:10:52 -0600

    inicio proyecto

---
+++ MServicioMoneda/src/main/java/com/example/msmoneda/MServicioMonedaApplication.java
@@ -0,0 +1,13 @@
+package com.example.msmoneda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MServicioMonedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MServicioMonedaApplication.class, args);
+	}
+
+}
---
+++ MServicioMoneda/src/test/java/com/example/msmoneda/MServicioMonedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msmoneda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MServicioMonedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
