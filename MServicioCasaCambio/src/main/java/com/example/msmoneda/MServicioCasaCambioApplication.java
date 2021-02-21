c38bb91292bf6bcfc84eebbdc65373455313d1bc
Ramo Mora Marquez <rmora.marquez@gmail.com>
2021-02-20 21:42:24 -0600

    Inicio MicroServicio Casa de Cambio

---
+++ MServicioCasaCambio/src/main/java/com/example/msmoneda/MServicioCasaCambioApplication.java
@@ -0,0 +1,13 @@
+package com.example.msmoneda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MServicioCasaCambioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MServicioCasaCambioApplication.class, args);
+	}
+
+}
---
+++ MServicioCasaCambio/src/test/java/com/example/msmoneda/MServicioCasaCambioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msmoneda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MServicioCasaCambioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
