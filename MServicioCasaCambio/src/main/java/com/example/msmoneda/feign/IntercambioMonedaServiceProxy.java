84306fd2845567d867ce2dfce8a689d5bfa22697
Ramo Mora Marquez <rmora.marquez@gmail.com>
2021-02-21 10:42:55 -0600

    Eureka Discovery Server y configuracion

--- MServicioCasaCambio/src/main/java/com/example/msmoneda/feign/IntercambioMonedaServiceProxy.java
+++ MServicioCasaCambio/src/main/java/com/example/msmoneda/feign/IntercambioMonedaServiceProxy.java
@@ -1,13 +1,17 @@
 package com.example.msmoneda.feign;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.msmoneda.modelo.IntercambioMonedaDto;
 
-@FeignClient(name="intercambio-service", url = "localhost:8000")
+//@FeignClient(name="intercambio-service", url = "localhost:8000")
+@FeignClient(name="intercambio-service")
+@RibbonClient(name = "intercambio-service")
 public interface IntercambioMonedaServiceProxy {
+	
 	@GetMapping("/intercambio/from/{origen}/to/{destino}")
 	IntercambioMonedaDto retrieveExchange(
 			@PathVariable("origen") String origen,
