f3dd35b4d192cb8894ff13d535c6d68a3a42ee28
Ramo Mora Marquez <rmora.marquez@gmail.com>
2021-02-21 10:53:45 -0600

    Eureka discovery server

---
+++ MServicioEurekaNaming/src/test/java/com/example/nameserver/MServicioEurekaNamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MServicioEurekaNamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
